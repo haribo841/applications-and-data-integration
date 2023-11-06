f6f5ad4fd7774f1394f11375ae7f6b282a57c386
haribo841 <adamwaldemarkubis@gmail.com>
2023-11-06 02:52:09 +0100

    Guess

--- Program.cs
+++ Program.cs
@@ -1,40 +1,153 @@
-﻿using Evaluation_task_3;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper.TypeConversion;
 using System;
 using System.Collections.Generic;
-namespace Evaluation_task_3
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        string intervalCsvPath = "C:\\Users\\Adam\\Desktop\\interval.csv";
+        string contractCsvPath = "C:\\Users\\Adam\\Desktop\\test.csv";
+        string outputCsvPath = "C:\\Users\\Adam\\Desktop\\output.csv";
+
+        var intervals = ReadIntervalsFromCsv(intervalCsvPath);
+        var contracts = ReadContractsFromCsv(contractCsvPath);
+
+        DateTime? startReportDate = intervals.Any() ? intervals.Min() : DateTime.UtcNow;
+        DateTime? endReportDate = intervals.Any() ? intervals.Max() : DateTime.UtcNow;
+
+        if (startReportDate == null || endReportDate == null)
         {
-            string intervalFilePath = "C:\\Users\\Adam\\Desktop\\interval.csv";
-            string contractFilePath = "C:\\Users\\Adam\\Desktop\\test.csv";
+            throw new Exception("Błąd");
+        }
 
-            IIntervalProvider intervalProvider = new CsvIntervalProvider();
-            Interval reportInterval = intervalProvider.GetIntervalFromCsv(intervalFilePath);
+        List<ReportRecord> reportIntervals = GenerateReportIntervals(startReportDate.Value, endReportDate.Value, contracts);
 
-            IContractProvider contractProvider = new CsvContractProvider();
-            List<Contract> contracts = contractProvider.GetContractsFromCsv(contractFilePath);
+        Console.WriteLine($"POCZĄTEK:\t\t\tKONIEC:");
 
-            ReportGenerator reportGenerator = new ReportGenerator();
-            List<string[]> report = reportGenerator.GenerateReport(contracts, reportInterval);
+        foreach (var interval in reportIntervals)
+        {
+            Console.WriteLine($"{interval.StartDate}\t{interval.EndDate}");
+        }
 
-            foreach (string[] intervalData in report)
+        SaveReportToCsv(reportIntervals, outputCsvPath);
+        Console.WriteLine("Raport został zapisany do pliku report.csv");
+    }
+
+    static List<DateTime> ReadIntervalsFromCsv(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            return csv.GetRecords<DateTime>().ToList();
+        }
+    }
+    static List<Contract> ReadContractsFromCsv(string filePath)
+    {
+        using (var reader = new StreamReader(filePath))
+        using (var csv = new CsvReader(reader, new CsvConfiguration(CultureInfo.InvariantCulture)))
+        {
+            return csv.GetRecords<Contract>().ToList();
+        }
+    }
+    static List<ReportRecord> GenerateReportIntervals(DateTime start, DateTime end, List<Contract> contracts)
+    {
+        List<ReportRecord> reportIntervals = new List<ReportRecord>();
+        DateTime currentIntervalStart = start;
+
+        foreach (var contract in contracts)
+        {
+            if (contract.StartDate.HasValue && contract.StartDate.Value > currentIntervalStart)
             {
-                string intervalStart = intervalData[0];
-                string intervalEnd = intervalData[1];
-                string contractId = intervalData[2];
-                string workTime = intervalData[3];
-
-                Console.WriteLine($"Początek: {intervalStart}");
-                Console.WriteLine($"Koniec: {intervalEnd}");
-                Console.WriteLine($"ID Kontraktu: {contractId}");
-                Console.WriteLine($"Czas Pracy: {workTime}");
-                Console.WriteLine();
+                reportIntervals.Add(new ReportRecord
+                {
+                    StartDate = currentIntervalStart.ToString("yyyy-MM-ddTHH:mm:ss zzz"),
+                    EndDate = contract.StartDate.Value.ToString("yyyy-MM-ddTHH:mm:ss zzz")
+                });
             }
+
+            if (contract.EndDate.HasValue)
+            {
+                currentIntervalStart = contract.EndDate.Value;
+            }
+        }
+
+        if (currentIntervalStart < end)
+        {
+            reportIntervals.Add(new ReportRecord
+            {
+                StartDate = currentIntervalStart.ToString("yyyy-MM-ddTHH:mm:ss zzz"),
+                EndDate = end.ToString("yyyy-MM-ddTHH:mm:ss zzz")
+            });
+        }
+
+        return reportIntervals;
+    }
+    public static void SaveReportToCsv(List<ReportRecord> reportData, string filePath)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = true, 
+        };
+
+        using (var writer = new StreamWriter(filePath))
+        using (var csv = new CsvWriter(writer, config))
+        {
+            csv.WriteRecords(reportData);
         }
     }
 }
+    class Contract
+{
+    [TypeConverter(typeof(CustomDateTimeConverter))]
+    public DateTime? StartDate { get; set; }
 
+    [TypeConverter(typeof(CustomDateTimeConverter))]
+    public DateTime? EndDate { get; set; }
+}
+public class NullableDateTimeConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (text == "-")
+        {
+            return null;
+        }
+        else
+        {
+            return base.ConvertFromString(text, row, memberMapData);
+        }
+    }
+}
+public class CustomDateTimeConverter : ITypeConverter
+{
+    public object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (DateTime.TryParse(text, out DateTime result))
+        {
+            return result;
+        }
+        return null;
+    }
+    public string ConvertToString(object value, IWriterRow row, MemberMapData memberMapData)
+    {
+        if (value is DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-ddTHH:mm:ss zzz");
+        }
+        return null;
+    }
+}
 
-
+class ReportRecord
+{
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Evaluation task 3
+﻿# Evaluation task 3
+
+## Jak to rozumiem
+
+<b>Zadanie polega na stworzeniu generatora raportów dla działu HR w pewnej firmie.
+Generator ma tworzyć raporty na dowolny okres czasu, podsumowywać czasy pracy pracownika w określonym przedziale czasowym, obsługiwać sytuacje, w których pracownik ma wiele kontraktów w danym okresie, a także okresy bez zatrudnienia.
+Kontrakty mogą być na czas nieokreślony, a czasy pracy powinny być sumowane osobno dla odpowiednich kontraktów lub okresów bez zatrudnienia.</b>
+niestety treść zadania jest sformułowana w sposób bardzo nie jasny
+
+##Pobranie danych z plików CSV:
+
+Kod wczytuje dane z dwóch plików CSV: "interval.csv" i "test.csv", które zawierają interwały czasowe oraz informacje o umowach z pracownikami.
+Dane z tych plików są wczytywane za pomocą biblioteki CsvHelper. W przypadku interwałów czasowych, dane są wczytywane jako listę dat (typ DateTime), a w przypadku umów pracowniczych, dane są wczytywane do obiektów klasy Contract.
+
+##Określenie daty początkowej i końcowej raportu:
+
+Na podstawie wczytanych interwałów czasowych obliczane są daty początkowe i końcowe raportu. Jeśli brak interwałów czasowych, używana jest aktualna data.
+Jeśli nie uda się określić dat początkowej i końcowej, program wyrzuca wyjątek.
+
+##Generowanie raportu:
+
+Następnie program generuje raport w postaci listy ReportRecord, która zawiera posortowane przedziały czasowe, w których będą sumowane czasy pracy pracowników.
+Iteruje przez umowy pracownicze (kontrakty) i tworzy przedziały czasowe, w których czasy pracy powinny być sumowane. Jeśli umowa ma datę rozpoczęcia późniejszą niż początek aktualnego przedziału, tworzony jest nowy przedział od początku aktualnego przedziału do daty rozpoczęcia umowy.
+Jeśli umowa ma datę zakończenia, aktualny przedział kończy się w tej dacie.
+Na koniec, jeśli aktualny przedział nie osiągnął daty końcowej raportu, tworzony jest przedział od aktualnego końca do daty końcowej raportu.
+
+##Zapis raportu do pliku CSV:
+
+Wygenerowany raport w postaci listy ReportRecord jest zapisywany do pliku "output.csv" za pomocą CsvHelper.
+Kod zawiera także konwersję dat z formatu tekstowego w plikach CSV za pomocą klasy CustomDateTimeConverter oraz obsługę daty niesprecyzowanej (null) za pomocą klasy NullableDateTimeConverter.
